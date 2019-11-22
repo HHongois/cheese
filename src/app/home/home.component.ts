e859142fcb7ecf392c81a1a3386ffb3f70b2fed5
HHongois <hubert.hongois@gmail.com>
2019-11-22 10:51:34 +0100

    debut

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+@Injectable()
+export class HomeComponent implements OnInit {
+
+  public data;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getConfig();
+  }
+
+  configUrl = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=fromagescsv-fromagescsv&facet=fromage';
+
+  getConfig() {
+    this.http.get(this.configUrl).subscribe((response) => this.data = response);
+  }
+
+}
